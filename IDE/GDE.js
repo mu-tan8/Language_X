3c6cc9ed3e7e0bb435b8d750be9aaf83656f085e
mu-tan8 <takka-y@hotmail.co.jp>
2017-07-28 13:59:40 +0900

    debugging now ..

--- IDE/GDE.js
+++ IDE/GDE.js
@@ -568,7 +568,7 @@ function preview(){
 	oDocument = oXML.document || oXML.contentDocument;
 
 	var oDOM = StrToXMLDOM(DocToStr(oDocument));
-console.log(oDOM);
+console.log(createDocument(oDOM,oXSL[1]));
 	alert(DocToStr(createDocument(oDOM,oXSL[1])));
 
 	oDOM = null;
